c2cf71daf9a01a8ab1815308b6b69b333aca38ae
Hemish123 <consultant@jmsadvisory.in>
2026-03-03 15:07:08 +0530

    Update settings.py

--- voice_bot/settings.py
+++ voice_bot/settings.py
@@ -204,7 +204,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/site/wwwroot/staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
